9515a9fb1acaaa98e6d937ca2130425ce20afde8
xiao11lang <1023819715@qq.com>
2021-12-02 21:09:59 +0800

    lint: add type for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "perf",
+        "feat",
+        "fix",
+        "refactor",
+        "docs",
+        "chore",
+        "style",
+        "revert",
+        "test",
+        "lint",
+      ],
+    ],
+  },
 };
